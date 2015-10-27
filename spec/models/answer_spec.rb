710d5851c11b1096d48e8623af65c20c4213306f
kelvey29 <kelvey29@gmail.com>
2015-10-27 00:25:56 +0000

    assignment-28-completed

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: "New Resolved") }
+  let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
+  
+  describe "attributes" do
+     it "should respond to body" do
+        expect(answer).to respond_to(:body) 
+     end
+  end
+end
