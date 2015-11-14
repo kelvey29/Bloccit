c08800792fa2a0e37c2bfcfca19941fc947c356f
kelvey29 <kelvey29@gmail.com>
2015-11-14 03:07:42 +0000

    Checkpoint-39-completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 include RandomData
  
+ # Create Users
+ 5.times do
+   user = User.create!(
+       name:     RandomData.random_name,
+       email:    RandomData.random_email,
+       password: RandomData.random_sentence
+       )
+   end
+ users = User.all
+
   # Create Topics
  15.times do
 
@@ -15,7 +25,8 @@ include RandomData
  50.times do
 
    Post.create!(
-
+     user: users.sample,
+     topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
@@ -32,7 +43,14 @@ include RandomData
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+     email: 'kelvey29@gmail.com',
+     password: 'helloworld'
+     )
+ 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
