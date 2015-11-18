a20833cd0f21a34f4561c0b466a2e6a76f0e905c
kelvey29 <kelvey29@gmail.com>
2015-11-18 04:34:14 +0000

    Checkpoint-43-completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,14 +24,17 @@ include RandomData
  # Create Posts
  50.times do
 
-   Post.create!(
+   post = Post.create!(
      user: users.sample,
      topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
- end
- posts = Post.all
+ 
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+  end
+   posts = Post.all
  
  # Create Comments
 
@@ -61,3 +64,4 @@ admin = User.create!(
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"
